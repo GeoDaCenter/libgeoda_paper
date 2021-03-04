1e7d7f3037248b78a8811161fb1cfbf662361c3f
Xun Li <xunli@uchicago.edu>
2021-03-04 09:18:11 -0700

    update rerun M1 results

--- rerun_chicago/run_pygeoda_perf.sh
+++ rerun_chicago/run_pygeoda_perf.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 echo "Test pyGeoDa local moran with Chicago_Parcels_Projected using permutation_method=complete"
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 complete 1
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 complete 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 complete 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 complete 1
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 complete 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 complete 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 complete 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 complete 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 complete 1
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 complete 1
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 complete 8
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 complete 8
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 complete 1
 echo "Test pyGeoDa local moran with Chicago_Parcels_Projected using permutation_method=lookup"
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 lookup 1
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 lookup 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 lookup 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 lookup 1
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 lookup 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 lookup 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 lookup 16
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 lookup 8
-python perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 lookup 1
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 lookup 1
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 999 lookup 8
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 lookup 1
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 9999 lookup 8
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 lookup 8
+python3 perf_pygeoda.py ../data/Chicago_parcels_projected.shp EstBuild 99999 lookup 1
